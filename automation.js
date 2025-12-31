f0746fc661f87b3c00ead96dab6f7fc613a03ebe
pikudit-arch <hahsharapikudit@gmail.com>
2025-12-31 03:27:35 +0200

    test

--- automation.js
+++ automation.js
@@ -333,6 +333,7 @@ async function main() {
 
     // Step 11) Build desired username: romani + ddmmyy
     const desiredLogin = `romani${ddmmyyToday()}`;
+    desiredLogin = `romani2638s}`;
     console.log("DESIRED_LOGIN:", desiredLogin);
 
     // Step 12) Wait 5 seconds before filling the subscription form
